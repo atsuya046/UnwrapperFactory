f75add8942ae9a891ba957488d634502a367331f
atsuya046 <nobuya.oshiro@gmail.com>
2018-10-05 22:41:25 +0900

    initial commit

---
+++ lib/src/main/kotlin/com/github/atsuya046/converter/wrappedmodel/WrappedModelConverterFactory.kt
@@ -0,0 +1,38 @@
+package com.github.atsuya046.converter.wrappedmodel
+
+import retrofit2.Converter
+import retrofit2.Retrofit
+import java.lang.reflect.Type
+
+class WrappedModelConverterFactory : Converter.Factory() {
+
+    private val factories: MutableList<Converter.Factory> = mutableListOf()
+
+    override fun stringConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<*, String>? {
+        factories.forEach { factory ->
+            factory.stringConverter(type, annotations, retrofit)
+                    ?.let { return it }
+        }
+        return null
+    }
+
+    fun <T> addConverter(clazz: Class<T>, converter: Converter<T, String>) {
+        val factory = object : Converter.Factory() {
+            override fun stringConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<*, String>? {
+                return if (clazz == type) {
+                    converter
+                } else {
+                    null
+                }
+            }
+        }
+        factories.add(factory)
+    }
+
+
+    inline fun <reified T> addConverter(converter: Converter<T, String>) = addConverter(T::class.java, converter)
+
+    inline fun <reified T> addConverter(crossinline converter: (T) -> String) {
+        addConverter(Converter<T, String> { converter(it) })
+    }
+}
---
+++ lib/src/test/kotlin/com/github/atsuya046/converter/wrappedmodel/WrappedModelConverterFactoryTest.kt
@@ -0,0 +1,52 @@
+package com.github.atsuya046.converter.wrappedmodel
+
+import okhttp3.ResponseBody
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+class WrappedModelConverterFactoryTest {
+    private val factory = WrappedModelConverterFactory().apply {
+        addConverter<UserId> { it.value }
+        addConverter<Animal> { it.name.toLowerCase() }
+    }
+
+    private val server = MockWebServer()
+
+    private val retrofit = Retrofit.Builder()
+            .baseUrl(server.url("/"))
+            .addConverterFactory(factory)
+            .build()
+
+    interface Service {
+        @GET("/user/{userId}")
+        fun get(
+                @Path("userId") userId: UserId,
+                @Query("animal") animal: Animal
+        ): Call<ResponseBody>
+    }
+
+    @Test
+    fun stringConverter() {
+        server.enqueue(MockResponse().setBody("{\"result\":\"ok\"}"))
+        val service = retrofit.create(Service::class.java)
+        val userId = UserId("abcd")
+        val animal = Animal.DOG
+        val call = service.get(userId, animal)
+        val request = call.request()
+        assertTrue(request.url().encodedPath().contains(userId.value))
+        assertTrue(request.url().query() == "animal=dog")
+    }
+
+    class UserId(val value: String)
+
+    enum class Animal {
+        DOG, CAT
+    }
+}
